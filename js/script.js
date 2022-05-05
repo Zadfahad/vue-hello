277d70f056e6d945671d4447377e7a3a7c4189e3
Zadfahad <fahadalam693@gmail.com>
2022-05-05 16:34:04 +0200

    done

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const app = new Vue (
+    {
+        el: '#root',
+        data:{
+            test: 'The killer mountain',
+            fairyMeadows: '../img/nanga-parbat.jpg'
+        }
+    }
+);
